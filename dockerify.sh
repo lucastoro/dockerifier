37bb6e24568a3a6e2960daa2965de70b35c2051f
Luca Castoro <13522333+lucastoro@users.noreply.github.com>
2019-06-08 04:37:02 +0200

    First commit

---
+++ dockerify.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+[ -z "$1" ] && {
+  echo "missing argument"
+  exit 1
+}
+
+BINARY=$(realpath $1) || exit 1
+
+[ -f $BINARY ] || {
+  echo "could not find $1"
+  exit 1
+}
+
+DIR=$(mktemp -d)
+pushd $DIR &>/dev/null
+
+echo "FROM scratch" > Dockerfile
+
+for file in $(ldd $BINARY | sed -r 's/[^ ]+ => ([^\(]+) \(0x.+/\1/g'); do
+  [ -f $file ] && {
+    cp $file .
+    echo "COPY $(basename $file) $(dirname $file)/" >> Dockerfile
+  }
+done
+
+cp $BINARY .
+echo "COPY $(basename $BINARY) /" >> Dockerfile
+echo "ENTRYPOINT [\"/$(basename $BINARY)\"]" >> Dockerfile
+
+docker build -t $(basename $BINARY) .
+
+popd &>/dev/null
+rm -rf $DIR
